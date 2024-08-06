fe155b576c5ae9beea31238b91da34a0832ea825
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-08-07 01:17:36 +0300

    baseTypes

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,20 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage<T>(message: T): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError() {
+function customError(): never {
   throw new Error('Error');
 }
+
+showMessage(43)
+calc(2, 4)
+customError()
 
 export {};
